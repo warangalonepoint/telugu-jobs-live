6389bf2ad18755e6966a8eec29f495922bda6b02
warangalonepoint <gullapallychaitanya@gmail.com>
2025-09-05 17:38:45 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,10 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      container: { center: true, padding: "1rem" }
-    }
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
