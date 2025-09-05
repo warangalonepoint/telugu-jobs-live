86c17e221beaf2195ee409b1e65792c47e707980
warangalonepoint <gullapallychaitanya@gmail.com>
2025-09-05 16:01:27 +0530

    Create districts.ts

---
+++ data/districts.ts
@@ -0,0 +1,7 @@
+export const DISTRICTS = [
+  "Warangal", "Hanamkonda", "Karimnagar", "Khammam",
+  "Nalgonda", "Nizamabad", "Sangareddy", "Medchal–Malkajgiri",
+  "Hyderabad", "Adilabad",
+  // AP samples:
+  "Visakhapatnam", "Vijayawada", "Guntur", "Tirupati", "Kurnool"
+];
