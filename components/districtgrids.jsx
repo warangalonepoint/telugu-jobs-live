fb0eacf3e6a291f6e3e3daadc0c28af40c16838e
warangalonepoint <gullapallychaitanya@gmail.com>
2025-09-06 10:08:43 +0530

    Update districtgrids.jsx

--- components/districtgrids.jsx
+++ components/districtgrids.jsx
@@ -1,13 +1,15 @@
 "use client";
 
-import JobCard from "./JobCard";
+import React from "react";
+import JobCard from "./jobcard"; // case matches file: jobcard.jsx
 
-export default function DistrictGrids({ sites }) {
+export default function DistrictGrids({ sites = [] }) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mt-6">
-      {sites.map((site) => (
-        <JobCard key={site.name} site={site} />
+      {sites.map((site, i) => (
+        <JobCard key={site?.id ?? site?.name ?? i} site={site} />
       ))}
     </div>
   );
 }
+
